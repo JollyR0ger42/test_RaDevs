a07febf65124f3a9bc4994a98420ceed1215bdc0
JollyR0ger <kav993@gmail.com>
2023-02-15 08:51:25 +0200

    add sass

--- svelte.config.js
+++ svelte.config.js
@@ -1,7 +1,9 @@
 import adapter from '@sveltejs/adapter-auto';
+import { sass } from 'svelte-preprocess-sass';
 
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
+	preprocess: {style: sass(),},
 	kit: {
 		adapter: adapter()
 	}
