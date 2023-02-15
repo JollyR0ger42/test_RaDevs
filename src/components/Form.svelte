243cee862c8df15c7d94cbd8c129d11ab95463be
JollyR0ger <kav993@gmail.com>
2023-02-15 08:22:51 +0200

    add bunch dumm component

---
+++ src/components/Form.svelte
@@ -0,0 +1,5 @@
+<script>
+  import Form1 from './Form1.svelte'
+</script>
+
+<Form1 />
---
+++ src/components/Form1.svelte
@@ -0,0 +1,26 @@
+<script>
+  let carBrand = ''
+  let zipCode = ''
+</script>
+
+<form class="form">
+  <label for="carBrand">
+    Car brand:
+    <input id="carBrand" bind:value={carBrand} />
+  </label>
+  <label for="zipCode">
+    Zip code:
+    <input id="zipCode" bind:value={zipCode} />
+  </label>
+</form>
+
+<style lang="scss">
+.form {
+  display: flex;
+  flex-direction: column;
+
+  & > *:not(:last-child) {
+    margin-bottom: 10px;
+  }
+}
+</style>
